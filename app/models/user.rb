4681f00452c3fa621da3d1b0ca4195d4e90f8b3d
Valdyn Hunt <valdyn@mac.com>
2016-03-18 17:42:55 -0700

    Just in case

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	has_many :todo_items, through: :todo_lists, source: :todo_items, dependent: :destroy
 
 	validates :username, presence: true
-
+    has_secure_password
 
 	def get_completed_count
       todo_items.where(completed: true).count
